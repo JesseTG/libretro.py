c40eda8e1a7f22547c65e29afd9df8d7630e897b
Jesse Talavera <jesse@jesse.tg>
2024-02-27 19:33:48 -0500

    Add a PixelFormat enum

--- src/libretro/defs.py
+++ src/libretro/defs.py
@@ -8,3 +8,9 @@ class Rotation(Enum):
     _90 = 1
     _180 = 2
     _270 = 3
+
+
+class PixelFormat(Enum):
+    RGB1555 = RETRO_PIXEL_FORMAT_0RGB1555
+    XRGB8888 = RETRO_PIXEL_FORMAT_XRGB8888
+    RGB565 = RETRO_PIXEL_FORMAT_RGB565
