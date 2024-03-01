f5c568e9a875581cbc61afaef85ed8a88eeea40b
Jesse Talavera <jesse@jesse.tg>
2024-03-01 14:12:01 -0500

    Flesh out `load_environment`

--- src/libretro/retro.py
+++ src/libretro/retro.py
@@ -1,27 +1,39 @@
+from ctypes import CDLL
+from collections.abc import *
+from typing import *
+
 from .audio import AudioState
 from .core import Core
 from .environment import Environment
 from .input import InputState
 from .video import VideoState
+from .defs import *
 
 
-def load_core(path: str) -> Environment:
+
+# TODO: Add ability to pass custom callbacks and content
+def load_environment(
+        core: str | CDLL,
+        content: Optional[Content | Sequence[Content]] = None
+) -> Environment:
     """
     Load a libretro core from a file and sets all the required retro_ callbacks.
+    Does not call retro_init or retro_load_game.
 
     Parameters:
-        path: The path to the core shared library.
+        core: The shared library that contains the core.
+          Can be a path to a file or a CDLL instance.
 
     Returns:
         An environment object that can be used to manage the core's life and execution.
 
-    Throws:
-        ValueError: If the core is missing a required symbol.
+    Raises:
+        ValueError: If `core` is missing a required symbol.
     """
     audio = AudioState()
     input_state = InputState()
     video = VideoState()
-    env = Environment(path, audio, input_state, video)
+    env = Environment(core, audio, input_state, video)
 
     env.core.set_video_refresh(env.video.refresh)
     env.core.set_audio_sample(env.audio.audio_sample)
@@ -31,3 +43,7 @@ def load_core(path: str) -> Environment:
     env.core.set_environment(env.environment)
 
     return env
+
+
+def init_environment(path: str) -> Environment:
+    pass
