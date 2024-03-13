58d82f1121d42850470ce27b1c06920bd6cc0d19
Jesse Talavera <jesse@jesse.tg>
2024-03-12 20:29:31 -0400

    Make `retro_get_proc_address_interface` callable directly

--- src/libretro/api/proc.py
+++ src/libretro/api/proc.py
@@ -1,10 +1,17 @@
 from ctypes import CFUNCTYPE, Structure
+from typing import AnyStr
 
 from .._utils import FieldsFromTypeHints, String, UNCHECKED
 
-retro_proc_address_t = CFUNCTYPE(None, )
+retro_proc_address_t = CFUNCTYPE(None)
 retro_get_proc_address_t = CFUNCTYPE(UNCHECKED(retro_proc_address_t), String)
 
 
 class retro_get_proc_address_interface(Structure, metaclass=FieldsFromTypeHints):
     get_proc_address: retro_get_proc_address_t
+
+    def __call__(self, sym: AnyStr) -> retro_proc_address_t:
+        if not self.get_proc_address:
+            raise ValueError("get_proc_address is NULL")
+
+        return self.get_proc_address(sym)
