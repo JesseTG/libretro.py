cc9b54591ec80defe9a9bbe7cf52a2700e007bfd
Jesse Talavera <jesse@jesse.tg>
2024-03-14 13:45:23 -0400

    Remove _utils._variadic_function
    - It's no longer needed

--- src/libretro/_utils.py
+++ src/libretro/_utils.py
@@ -447,30 +447,6 @@ def UNCHECKED(type):
         return c_void_p
 
 
-# ctypes doesn't have direct support for variadic functions, so we have to write
-# our own wrapper class
-class _variadic_function(object):
-    def __init__(self, func, restype, argtypes, errcheck):
-        self.func = func
-        self.func.restype = restype
-        self.argtypes = argtypes
-        if errcheck:
-            self.func.errcheck = errcheck
-
-    def _as_parameter_(self):
-        # So we can pass this variadic function as a function pointer
-        return self.func
-
-    def __call__(self, *args):
-        fixed_args = []
-        i = 0
-        for argtype in self.argtypes:
-            # Typecheck what we can
-            fixed_args.append(argtype.from_param(args[i]))
-            i += 1
-        return self.func(*fixed_args + list(args[i:]))
-
-
 def ord_if_char(value):
     """
     Simple helper used for casts to simple builtin types:  if the argument is a
