4c9abd9df69e9a7be085172ffe9d81670e4e189c
Jesse Talavera <jesse@jesse.tg>
2024-03-20 11:44:35 -0400

    Move some video-related definitions to defs.py

--- src/libretro/api/video/__init__.py
+++ src/libretro/api/video/__init__.py
@@ -1,62 +1,11 @@
 from abc import abstractmethod
 from array import array
-from enum import IntEnum
-from ctypes import *
+from ctypes import c_void_p, memmove
 from typing import Protocol, runtime_checkable, final
 
 from ..system import retro_system_av_info, retro_game_geometry
-from ...h import *
-from ..._utils import FieldsFromTypeHints
+from .defs import *
 
-retro_video_refresh_t = CFUNCTYPE(None, c_void_p, c_uint, c_uint, c_size_t)
-
-retro_usec_t = c_int64
-retro_frame_time_callback_t = CFUNCTYPE(None, retro_usec_t)
-
-
-class retro_frame_time_callback(Structure, metaclass=FieldsFromTypeHints):
-    callback: retro_frame_time_callback_t
-    reference: retro_usec_t
-
-
-class Rotation(IntEnum):
-    NONE = 0
-    NINETY = 1
-    ONE_EIGHTY = 2
-    TWO_SEVENTY = 3
-
-    def __init__(self, value):
-        self._type_ = 'I'
-
-
-class PixelFormat(IntEnum):
-    RGB1555 = RETRO_PIXEL_FORMAT_0RGB1555
-    XRGB8888 = RETRO_PIXEL_FORMAT_XRGB8888
-    RGB565 = RETRO_PIXEL_FORMAT_RGB565
-
-    def __init__(self, value):
-        self._type_ = 'I'
-
-    @property
-    def bytes_per_pixel(self) -> int:
-        match self:
-            case self.RGB1555:
-                return 2
-            case self.XRGB8888:
-                return 4
-            case self.RGB565:
-                return 2
-            case _:
-                raise ValueError(f"Unknown pixel format: {self}")
-
-class retro_framebuffer(Structure, metaclass=FieldsFromTypeHints):
-    data: c_void_p
-    width: c_uint
-    height: c_uint
-    pitch: c_size_t
-    format: retro_pixel_format
-    access_flags: c_uint
-    memory_flags: c_uint
 
 @runtime_checkable
 class VideoCallbacks(Protocol):
