5e11fd2fa4a0798ec51fc02b94ea6abb1fcc4da1
Jesse Talavera <jesse@jesse.tg>
2024-03-21 20:49:59 -0400

    Move the LED interface declarations to a package

--- src/libretro/api/led/defs.py
+++ src/libretro/api/led/defs.py
@@ -1,9 +1,14 @@
 from ctypes import CFUNCTYPE, c_int, Structure
+from dataclasses import dataclass
 
-from .._utils import FieldsFromTypeHints
+from ..._utils import FieldsFromTypeHints
 
 retro_set_led_state_t = CFUNCTYPE(None, c_int, c_int)
 
 
+@dataclass(init=False)
 class retro_led_interface(Structure, metaclass=FieldsFromTypeHints):
     set_led_state: retro_set_led_state_t
+
+
+__all__ = ['retro_led_interface', 'retro_set_led_state_t']
