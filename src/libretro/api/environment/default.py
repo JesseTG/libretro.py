5b0fa35ff6e68a81330f532b87e14c8f524c4878
Jesse Talavera <jesse@jesse.tg>
2024-04-01 15:59:39 -0400

    Revert "Move the _overridable_envcalls in EnvironmentDriver down to DefaultEnvironmentDriver"
    This reverts commit 3aed274d24c99ed574a9a6c667ab5e93bfe2cbf6.

--- src/libretro/api/environment/default.py
+++ src/libretro/api/environment/default.py
@@ -4,12 +4,13 @@ from typing import override
 
 from .dict import DictEnvironmentDriver
 from .defs import *
-from ..audio import *
+from ..audio.callback import retro_audio_callback, retro_audio_buffer_status_callback
 from ..av.defs import retro_game_geometry, retro_system_av_info
 from ..camera import retro_camera_callback
 from ..content.defs import retro_subsystem_info, retro_game_info_ext, retro_system_content_info_override
 from ..disk import retro_disk_control_callback, retro_disk_control_ext_callback
-from ..input import *
+from ..input.info import retro_controller_info, retro_input_descriptor
+from ..input.keyboard import retro_keyboard_callback
 from ..led.defs import retro_led_interface
 from ..location.defs import retro_location_callback
 from ..log.defs import retro_log_callback
@@ -22,6 +23,8 @@ from ..options.defs import *
 from ..perf.defs import retro_perf_callback
 from ..power import retro_device_power
 from ..proc import retro_get_proc_address_interface
+from ..rumble.defs import retro_rumble_interface
+from ..sensor.defs import retro_sensor_interface
 from ..throttle import retro_throttle_state, retro_fastforwarding_override
 from ..vfs.defs import retro_vfs_interface_info
 from ..video.defs import *
@@ -31,7 +34,6 @@ from ..video.negotiate.defs import *
 from ...h import *
 
 
-# noinspection PyMethodMayBeStatic,PyUnusedLocal,PyShadowingNames
 class DefaultEnvironmentDriver(DictEnvironmentDriver):
     @override
     def __init__(self):
@@ -97,7 +99,7 @@ class DefaultEnvironmentDriver(DictEnvironmentDriver):
             self._get_rumble_interface(cast(data, POINTER(retro_rumble_interface))),
 
             EnvironmentCall.GET_INPUT_DEVICE_CAPABILITIES: lambda data:
-            self._get_input_device_capabilities(cast(data, POINTER(c_uint64))),
+            self._get_input_device_capabilites(cast(data, POINTER(c_uint64))),
 
             EnvironmentCall.GET_SENSOR_INTERFACE: lambda data:
             self._get_sensor_interface(cast(data, POINTER(retro_sensor_interface))),
@@ -273,154 +275,3 @@ class DefaultEnvironmentDriver(DictEnvironmentDriver):
 
         super().__init__(envcalls)
 
-    def _set_rotation(self, rotation: POINTER(c_uint)) -> bool: return False
-
-    def _get_overscan(self, overscan: POINTER(c_bool)) -> bool: return False
-
-    def _get_can_dupe(self, can_dupe: POINTER(c_bool)) -> bool: return False
-
-    def _set_message(self, message: POINTER(retro_message)) -> bool: return False
-
-    def _shutdown(self) -> bool: return False
-
-    def _set_performance_level(self, level: POINTER(c_uint)) -> bool: return False
-
-    def _get_system_directory(self, dir: POINTER(c_char_p)) -> bool: return False
-
-    def _set_pixel_format(self, fmt: POINTER(retro_pixel_format)) -> bool: return False
-
-    def _set_input_descriptors(self, descriptors: POINTER(retro_input_descriptor)) -> bool: return False
-
-    def _set_keyboard_callback(self, callback: POINTER(retro_keyboard_callback)) -> bool: return False
-
-    def _set_disk_control_interface(self, callback: POINTER(retro_disk_control_callback)) -> bool: return False
-
-    def _set_hw_render(self, data: POINTER(retro_hw_render_callback)) -> bool: return False
-
-    def _get_variable(self, variable: POINTER(retro_variable)) -> bool: return False
-
-    def _set_variables(self, variables: POINTER(retro_variable)) -> bool: return False
-
-    def _get_variable_update(self, update: POINTER(c_bool)) -> bool: return False
-
-    def _set_support_no_game(self, support: POINTER(c_bool)) -> bool: return False
-
-    def _get_libretro_path(self, path: POINTER(c_char_p)) -> bool: return False
-
-    def _set_frame_time_callback(self, callback: POINTER(retro_frame_time_callback)) -> bool: return False
-
-    def _set_audio_callback(self, callback: POINTER(retro_audio_callback)) -> bool: return False
-
-    def _get_rumble_interface(self, data: POINTER(retro_rumble_interface)) -> bool: return False
-
-    def _get_input_device_capabilities(self, capabilities: POINTER(c_uint64)) -> bool: return False
-
-    def _get_sensor_interface(self, interface: POINTER(retro_sensor_interface)) -> bool: return False
-
-    def _get_camera_interface(self, interface: POINTER(retro_camera_callback)) -> bool: return False
-
-    def _get_log_interface(self, interface: POINTER(retro_log_callback)) -> bool: return False
-
-    def _get_perf_interface(self, interface: POINTER(retro_perf_callback)) -> bool: return False
-
-    def _get_location_interface(self, interface: POINTER(retro_location_callback)) -> bool: return False
-
-    def _get_core_assets_directory(self, dir: POINTER(c_char_p)) -> bool: return False
-
-    def _get_save_directory(self, dir: POINTER(c_char_p)) -> bool: return False
-
-    def _set_system_av_info(self, info: POINTER(retro_system_av_info)) -> bool: return False
-
-    def _set_proc_address_callback(self, callback: POINTER(retro_get_proc_address_interface)) -> bool: return False
-
-    def _set_subsystem_info(self, info: POINTER(retro_subsystem_info)) -> bool: return False
-
-    def _set_controller_info(self, info: POINTER(retro_controller_info)) -> bool: return False
-
-    def _set_memory_maps(self, maps: POINTER(retro_memory_map)) -> bool: return False
-
-    def _set_geometry(self, geometry: POINTER(retro_game_geometry)) -> bool: return False
-
-    def _get_username(self, username: POINTER(c_char_p)) -> bool: return False
-
-    def _get_language(self, language: POINTER(retro_language)) -> bool: return False
-
-    def _get_current_software_framebuffer(self, framebuffer: POINTER(retro_framebuffer)) -> bool: return False
-
-    def _get_hw_render_interface(self, interface: POINTER(retro_hw_render_interface)) -> bool: return False
-
-    def _set_support_achievements(self, support: POINTER(c_bool)) -> bool: return False
-
-    def _set_hw_render_context_negotiation_interface(self, interface: POINTER(retro_hw_render_context_negotiation_interface)) -> bool: return False
-
-    def _set_serialization_quirks(self, quirks: POINTER(c_uint64)) -> bool: return False
-
-    def _set_hw_shared_context(self) -> bool: return False
-
-    def _get_vfs_interface(self, vfs: POINTER(retro_vfs_interface_info)) -> bool: return False
-
-    def _get_led_interface(self, led: POINTER(retro_led_interface)) -> bool: return False
-
-    def _get_audio_video_enable(self, enable: POINTER(retro_av_enable_flags)) -> bool: return False
-
-    def _get_midi_interface(self, midi: POINTER(retro_midi_interface)) -> bool: return False
-
-    def _get_fastforwarding(self, is_fastforwarding: POINTER(c_bool)) -> bool: return False
-
-    def _get_target_refresh_rate(self, rate: POINTER(c_float)) -> bool: return False
-
-    def _get_input_bitmasks(self) -> bool: return False
-
-    def _get_core_options_version(self, version: POINTER(c_uint)) -> bool: return False
-
-    def _set_core_options(self, options: POINTER(retro_core_option_definition)) -> bool: return False
-
-    def _set_core_options_intl(self, options: POINTER(retro_core_options_intl)) -> bool: return False
-
-    def _set_core_options_display(self, options: POINTER(retro_core_option_display)) -> bool: return False
-
-    def _get_preferred_hw_render(self, preferred: POINTER(retro_hw_context_type)) -> bool: return False
-
-    def _get_disk_control_interface_version(self, version: POINTER(c_uint)) -> bool: return False
-
-    def _set_disk_control_ext_interface(self, interface: POINTER(retro_disk_control_ext_callback)) -> bool: return False
-
-    def _get_message_interface_version(self, version: POINTER(c_uint)) -> bool: return False
-
-    def _set_message_ext(self, interface: POINTER(retro_message_ext)) -> bool: return False
-
-    def _get_input_max_users(self, max_users: POINTER(c_uint)) -> bool: return False
-
-    def _set_audio_buffer_status_callback(self, callback: POINTER(retro_audio_buffer_status_callback)) -> bool: return False
-
-    def _set_minimum_audio_latency(self, latency: POINTER(c_uint)) -> bool: return False
-
-    def _set_fastforwarding_override(self, override: POINTER(retro_fastforwarding_override)) -> bool: return False
-
-    def _set_content_info_override(self, override: POINTER(retro_system_content_info_override)) -> bool: return False
-
-    def _get_game_info_ext(self, info: POINTER(POINTER(retro_game_info_ext))) -> bool: return False
-
-    def _set_core_options_v2(self, options: POINTER(retro_core_options_v2)) -> bool: return False
-
-    def _set_core_options_v2_intl(self, options: POINTER(retro_core_options_v2_intl)) -> bool: return False
-
-    def _set_core_options_update_display_callback(self, callback: POINTER(retro_core_options_update_display_callback)) -> bool: return False
-
-    def _set_variable(self, variable: POINTER(retro_variable)) -> bool: return False
-
-    def _get_throttle_state(self, state: POINTER(retro_throttle_state)) -> bool: return False
-
-    def _get_savestate_context(self, context: POINTER(retro_savestate_context)) -> bool: return False
-
-    def _get_hw_render_context_negotiation_interface_support(self, support: POINTER(retro_hw_render_context_negotiation_interface)) -> bool: return False
-
-    def _get_jit_capable(self, capable: POINTER(c_bool)) -> bool: return False
-
-    def _get_microphone_interface(self, interface: POINTER(retro_microphone_interface)) -> bool: return False
-
-    def _get_device_power(self, power: POINTER(retro_device_power)) -> bool: return False
-
-    def _set_netpacket_interface(self, interface: POINTER(retro_netpacket_callback)) -> bool: return False
-
-    def _get_playlist_directory(self, dir: POINTER(c_char_p)) -> bool: return False
