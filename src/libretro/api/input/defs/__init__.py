78aa9e16cf75ff28434def2bfce6f188cdaa89b8
Jesse Talavera <jesse@jesse.tg>
2024-04-06 14:11:14 -0400

    Expose each input def via libretro.api.input.defs
    - So that "pointer" doesn't result in a name clash with ctypes

--- src/libretro/api/input/defs/__init__.py
+++ src/libretro/api/input/defs/__init__.py
@@ -5,3 +5,14 @@ from .keyboard import *
 from .lightgun import *
 from .mouse import *
 from .pointer import *
+
+
+__all__ = [
+    *analog.__all__,
+    *device.__all__,
+    *joypad.__all__,
+    *keyboard.__all__,
+    *lightgun.__all__,
+    *mouse.__all__,
+    *pointer.__all__,
+]
