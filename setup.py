463b0533d36885f8cda7004f1f51eca21b6098ad
Jesse Talavera <jesse@jesse.tg>
2024-04-11 21:47:55 -0400

    Fetch the version dynamically from `CHANGELOG.md`

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+from os.path import join, dirname
+import re
 import setuptools
 
-# TODO: Fetch the version dynamically from CHANGELOG.md
+with open(join(dirname(__file__), "CHANGELOG.md"), "r") as file:
+    changelog = file.read()
 
-setuptools.setup()
+    # We fetch the version number from CHANGELOG.md
+    # so that we don't have to maintain it in multiple places.
+    match = re.search(r"## \[(?P<version>\d+\.\d+\.\d+)]", changelog, re.MULTILINE)
+    if not match:
+        raise ValueError("Could not find the latest version in CHANGELOG.md")
+
+    version = match['version']
+
+setuptools.setup(version=version)
