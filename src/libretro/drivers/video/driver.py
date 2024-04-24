95231bc70207e69c17cdfc2807abb3e0f2f21432
Jesse Talavera <jesse@jesse.tg>
2024-04-24 14:24:08 -0400

    Add API documentation for `VideoDriver`

--- src/libretro/drivers/video/driver.py
+++ src/libretro/drivers/video/driver.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 from collections.abc import Set
+from enum import Enum
 from typing import Protocol, runtime_checkable
 
 from libretro.api.av import retro_game_geometry, retro_system_av_info
@@ -15,23 +16,79 @@ from libretro.api.video import (
 )
 
 
+class FrameBufferSpecial(Enum):
+    DUPE = None
+    HARDWARE = -1
+
+
 @runtime_checkable
 class VideoDriver(Protocol):
     @abstractmethod
-    def refresh(self, data: memoryview | None, width: int, height: int, pitch: int) -> None:
+    def refresh(self, data: memoryview | FrameBufferSpecial, width: int, height: int, pitch: int) -> None:
+        """
+        Updates the framebuffer with the given video data.
+        This method is exposed to the core through ``retro_set_video_refresh``.
+
+        :param data: One of the following:
+
+            - A ``memoryview`` pointing to pixel data
+              in the format given by ``self.pixel_format``.
+              Should be read-only.
+            - ``FrameBufferSpecial.DUPE`` if the frontend should re-render the previous frame.
+            - ``FrameBufferSpecial.HARDWARE`` if the frontend should render the frame
+              using the active hardware context.
+
+        :param width: The width of the frame in ``data``, in pixels.
+        :param height: The height of the frame in ``data``, in pixels.
+        :param pitch: The width of the frame in ``data``, in bytes.
+        :raises TypeError: If any parameter's type is not consistent with this method's signature.
+        :raises ValueError: If ``data`` is a ``memoryview``
+            and its length is not equal to ``pitch * height``.
+        """
         ...
 
+    @property
     @abstractmethod
     def supported_contexts(self) -> Set[HardwareContext]:
+        """
+        The set of all hardware contexts supported by this driver.
+        All video drivers must support at least ``HardwareContext.NONE``,
+        which indicates software rendering capabilities.
+
+        Most drivers will only support a single context type,
+        but they can support more than one.
+        """
         ...
 
+    @property
     @abstractmethod
-    def active_context(self) -> HardwareContext | None:
+    def active_context(self) -> HardwareContext:
+        """
+        The hardware context that is currently exposed to the core.
+        Must be a member of ``self.supported_contexts``.
+
+        Will be ``HardwareContext.NONE`` until the core
+        successfully requests a hardware context using ``EnvironmentCall.SET_HW_RENDER``,
+        at which point it will be set to the requested context type.
+        """
         ...
 
     @property
     @abstractmethod
     def preferred_context(self) -> HardwareContext | None:
+        """
+        The preferred hardware context for this driver.
+        Corresponds to ``EnvironmentCall.GET_PREFERRED_HW_RENDER``
+
+        Must be a member of ``self.supported_contexts``,
+        or ``None`` to indicate that ``EnvironmentCall.GET_PREFERRED_HW_RENDER`` is unavailable.
+
+        Setting or deleting this will not affect the active context.
+
+        :raise NotImplementedError: If attempting to set a preferred context
+            that this driver doesn't support.
+        :raise TypeError: If attempting to set a non-``HardwareContext`` value.
+        """
         ...
 
     @preferred_context.setter
@@ -46,19 +103,45 @@ class VideoDriver(Protocol):
 
     @abstractmethod
     def set_context(self, callback: retro_hw_render_callback) -> retro_hw_render_callback | None:
+        """
+        Corresponds to ``EnvironmentCall.SET_HW_RENDER``.
+
+        :param callback:
+        :raises TypeError: If ``callback`` is not a ``retro_hw_render_callback``.
+        :return: If the request in ``callback`` is accepted, a new ``retro_hw_render_callback``
+          with values and pointers that the core can use to interact with the hardware context.
+          Otherwise, ``None``.
+        """
         ...
 
     @abstractmethod
     def context_reset(self) -> None:
+        """
+        Initializes the hardware context
+        and calls the core's registered ``context_reset`` callback.
+        """
         ...
 
     @abstractmethod
     def context_destroy(self) -> None:
+        """
+        Destroys the hardware context
+        and calls the core's registered ``retro_hw_render_callback.context_destroy`` callback.
+        """
         ...
 
     @property
     @abstractmethod
     def rotation(self) -> Rotation:
+        """
+        Get the rotation of the video output.
+
+        If the video driver doesn't support rotation,
+        then this property should return ``Rotation.NONE``.
+
+        :raise UnsupportedEnvCall: If setting a rotation
+          and this driver doesn't support ``EnvironmentCall.SET_ROTATION``.
+        """
         ...
 
     @rotation.setter
@@ -69,11 +152,24 @@ class VideoDriver(Protocol):
     @property
     @abstractmethod
     def can_dupe(self) -> bool:
+        """
+        Whether the frontend can re-render the previous frame.
+
+        :note: Drivers may override this property to give it a setter.
+
+        :raises UnsupportedEnvCall: If this driver doesn't support ``EnvironmentCall.CAN_DUPE``.
+        """
         ...
 
     @property
     @abstractmethod
     def pixel_format(self) -> PixelFormat:
+        """
+        The pixel format that this driver uses for the frame buffer.
+
+        :raise ValueError: If trying to set an invalid ``PixelFormat``.
+        :raise UnsupportedEnvCall: If this driver doesn't support ``EnvironmentCall.SET_PIXEL_FORMAT``.
+        """
         ...
 
     @pixel_format.setter
@@ -143,4 +239,5 @@ class VideoDriver(Protocol):
 
 __all__ = [
     "VideoDriver",
+    "FrameBufferSpecial",
 ]
