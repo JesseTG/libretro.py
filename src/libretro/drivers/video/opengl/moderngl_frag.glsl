58da57c421a60e81ec1e3312238d682fd3724edf
Jesse Talavera <jesse@jesse.tg>
2024-05-08 09:06:00 -0400

    Whoops, `texture` is a built-in function name

--- src/libretro/drivers/video/opengl/moderngl_frag.glsl
+++ src/libretro/drivers/video/opengl/moderngl_frag.glsl
@@ -1,9 +1,9 @@
 #version 330
 
-uniform sampler2D texture;
+uniform sampler2D screenTexture;
 in vec2 transformedTexCoord;
 out vec4 pixelColor;
 
 void main() {
-    pixelColor = vec4(texture(texture, transformedTexCoord).rgb, 1.0f);
+    pixelColor = vec4(texture(screenTexture, transformedTexCoord).rgb, 1.0f);
 }
