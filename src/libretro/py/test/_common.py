48e7548d0262fd4945407b6652f75522b1c4573f
Jesse Talavera <jesse@jesse.tg>
2024-09-06 11:49:29 -0400

    Print less verbose errors when loading a core fails

--- src/libretro/py/test/_common.py
+++ src/libretro/py/test/_common.py
@@ -1,13 +1,26 @@
 from typing import Annotated
 
+from click import BadArgumentUsage, FileError
 from typer import Argument, Option
 
 from libretro import Core
 
+
+def load_core(value: str):
+    try:
+        return Core(value)
+    except FileNotFoundError as e:
+        raise FileError(value, "File not found") from e
+    except OSError as e:
+        raise FileError(value, f"{e}") from e
+    except ValueError as e:
+        raise BadArgumentUsage(f"{e}") from e
+
+
 CoreArg = Annotated[
     Core,
     Argument(
-        parser=Core,
+        parser=load_core,
         help="Path to the libretro core to load. Must be a complete path, not a short name.",
         show_default=False,
         metavar="core",
