0b7bccb8356ea83e8f7aaf59c13463909fd0120d
Jesse Talavera <jesse@jesse.tg>
2024-09-16 11:30:59 -0400

    Add CLI options for setting framecount and core options

--- src/libretro/py/test/_common.py
+++ src/libretro/py/test/_common.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from typing import Annotated
 
@@ -16,6 +17,17 @@ def load_core(value: str):
         raise FileError(value, f"{e}") from e
     except ValueError as e:
         raise BadArgumentUsage(f"{e}") from e
+
+
+def option_callback(value: list[str] | None):
+    if value is None:
+        return None
+
+    for v in value:
+        if not re.match(r"^[a-zA-Z0-9_\-]+=.+$", v):
+            raise BadArgumentUsage(f"Invalid option format: {v}")
+
+    return value
 
 
 CoreArg = Annotated[
@@ -48,9 +60,31 @@ SubsystemOption = Annotated[
 
 VerboseOption = Annotated[int, Option("--verbose", "-v", count=True)]
 
+FrameCountOption = Annotated[
+    int,
+    Option(
+        "--frames",
+        "-n",
+        help="The number of frames to run the core for. May exit earlier if the core exits explicitly; this is not necessarily an error.",
+        min=0,
+    ),
+]
+
+CoreOptionsOption = Annotated[
+    list[str] | None,
+    Option(
+        "--option",
+        "-O",
+        callback=option_callback,
+        help="Set an option for the core. Format: 'option=value'. May be specified multiple times.",
+    ),
+]
+
 __all__ = [
     "CoreArg",
     "VerboseOption",
     "ContentArg",
     "SubsystemOption",
+    "FrameCountOption",
+    "CoreOptionsOption",
 ]
