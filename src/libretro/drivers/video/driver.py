9602f35ede8aa2fcd2413ee18b847681afd81d38
Jesse Talavera <jesse@jesse.tg>
2024-09-16 15:03:46 -0400

    Touch up the `VideoDriver` docs

--- src/libretro/drivers/video/driver.py
+++ src/libretro/drivers/video/driver.py
@@ -1,3 +1,7 @@
+"""
+Types and classes for defining how a core renders its graphics.
+"""
+
 from abc import abstractmethod
 from collections.abc import Set
 from enum import Enum
@@ -30,6 +34,14 @@ class Screenshot(NamedTuple):
 
 @runtime_checkable
 class VideoDriver(Protocol):
+    """
+    Describes functionality for rendering video output,
+    including essential libretro callbacks
+    and environment calls.
+
+    Some of these may not be implemented by all drivers.
+    """
+
     @abstractmethod
     def refresh(
         self, data: memoryview | FrameBufferSpecial, width: int, height: int, pitch: int
@@ -40,19 +52,27 @@ class VideoDriver(Protocol):
 
         :param data: One of the following:
 
-            - A ``memoryview`` pointing to pixel data
-              in the format given by ``self.pixel_format``.
-              Should be read-only.
-            - ``FrameBufferSpecial.DUPE`` if the frontend should re-render the previous frame.
-            - ``FrameBufferSpecial.HARDWARE`` if the frontend should render the frame
-              using the active hardware context.
+            :class:`memoryview`
+                Contains pixel data in the format given by :attr:`pixel_format`.
+                Should be read-only.
+
+            :attr:`FrameBufferSpecial.DUPE`
+                If the frontend should re-render the previous frame.
+
+            :attr:`FrameBufferSpecial.HARDWARE`
+                If the frontend should render the frame
+                using the active graphics API context.
 
         :param width: The width of the frame in ``data``, in pixels.
         :param height: The height of the frame in ``data``, in pixels.
         :param pitch: The width of the frame in ``data``, in bytes.
         :raises TypeError: If any parameter's type is not consistent with this method's signature.
-        :raises ValueError: If ``data`` is a ``memoryview``
+        :raises ValueError: If ``data`` is a :class:`memoryview`
             and its length is not equal to ``pitch * height``.
+
+        .. note::
+
+            Corresponds to :type:`.retro_video_refresh_t`.
         """
         ...
 
@@ -60,15 +80,39 @@ class VideoDriver(Protocol):
     @abstractmethod
     def needs_reinit(self) -> bool:
         """
-        Whether the driver must be reinitialized.
+        :obj:`True` if this video driver needs to be reinitialized,
+        either because the core requested it or because of internal changes.
+        Can also indicate that the driver hasn't yet been initialized at all.
+
+        .. warning::
+
+            :class:`.VideoDriver` implementations aren't necessarily initialized in their constructors.
         """
         ...
 
     @abstractmethod
     def reinit(self) -> None:
         """
-        Reinitializes the driver.
-        Calls context_reset() and context_destroy() as needed.
+        Reinitializes the video driver,
+        applying any changes made to it since the last call to this method.
+
+        The video driver will perform the following steps:
+
+        1. Call the registered :attr:`.retro_hw_render_callback.context_destroy` (if any)
+           to tell the core to release any resources associated with the current graphics API.
+        2. Release all libretro.py-managed graphics resources.
+        3. Release the graphics context if switching to a different graphics API
+           or making changes that require a new context.
+        4. Initialize a new graphics context if necessary.
+        5. Initialize internal resources for libretro.py.
+        6. Call the registered :attr:`.retro_hw_render_callback.context_reset` (if any)
+           to tell the core it can initialize its graphics API resources.
+
+
+        .. tip::
+
+            The purpose of :attr:`.retro_hw_render_callback.cache_context`
+            is to tell libretro.py to do its best to avoid calling this method.
         """
         ...
 
@@ -76,12 +120,15 @@ class VideoDriver(Protocol):
     @abstractmethod
     def supported_contexts(self) -> Set[HardwareContext]:
         """
-        The set of all hardware contexts supported by this driver.
-        All video drivers must support at least ``HardwareContext.NONE``,
+        The set of all graphics APIs supported by this driver.
+        All video drivers must support at least :attr:`.HardwareContext.NONE`,
         which indicates software rendering capabilities.
 
-        Most drivers will only support a single context type,
-        but they can support more than one.
+        .. tip::
+
+            Most drivers won't support more than one kind of graphics context
+            (not counting :attr:`~.HardwareContext.NONE`),
+            but this isn't a hard requirement.
         """
         ...
 
@@ -90,11 +137,25 @@ class VideoDriver(Protocol):
     def active_context(self) -> HardwareContext:
         """
         The hardware context that is currently exposed to the core.
-        Must be a member of ``self.supported_contexts``.
+        Must be an element of :attr:`~.VideoDriver.supported_contexts`.
 
-        Will be ``HardwareContext.NONE`` until the core
-        successfully requests a hardware context using ``EnvironmentCall.SET_HW_RENDER``,
+        Will be :attr:`.HardwareContext.NONE` until the core
+        successfully requests a graphics context using :attr:`.EnvironmentCall.SET_HW_RENDER`,
         at which point it will be set to the requested context type.
+
+        .. attention::
+
+            This property *does not* necessarily indicate which graphics API is loaded and active;
+            it specifically means the context type that the core **requested**.
+
+            For example, a core that uses software rendering
+            wouldn't request a hardware context,
+            therefore this property would return :attr:`.HardwareContext.NONE`
+            regardless of the actual graphics API in use.
+
+            After all, most of RetroArch's video drivers rely on hardware graphics APIs,
+            even if the core doesn't request them.
+            If the core is only software-rendered, then what difference does it make?
         """
         ...
 
@@ -103,16 +164,23 @@ class VideoDriver(Protocol):
     def preferred_context(self) -> HardwareContext | None:
         """
         The preferred hardware context for this driver.
-        Corresponds to ``EnvironmentCall.GET_PREFERRED_HW_RENDER``.
+        Cores that support multiple graphics APIs
+        can use this to choose one without the player needing to specify it.
 
-        Must be a member of ``self.supported_contexts``,
-        or ``None`` to indicate that ``EnvironmentCall.GET_PREFERRED_HW_RENDER`` is unavailable.
+        Can be a member of :attr:`~.VideoDriver.supported_contexts`
+        (including :attr:`~.HardwareContext.NONE`)
+        to indicate preference for that context,
+        or :obj:`None` to disable :attr:`.EnvironmentCall.GET_PREFERRED_HW_RENDER`.
 
-        Setting or deleting this will not affect the active context.
+        Setting or deleting this property will not affect the active context.
 
-        :raise NotImplementedError: If attempting to set a preferred context
-            that this driver doesn't support.
-        :raise TypeError: If attempting to set a non-``HardwareContext`` value.
+        :raise ValueError: If attempting to set a preferred context
+            that this driver doesn't support (i.e. that's not in :attr:`~.VideoDriver.supported_contexts`).
+        :raise TypeError: If attempting to set a value that isn't a :class:`.HardwareContext`.
+
+        .. note::
+
+            Corresponds to ``RETRO_ENVIRONMENT_GET_PREFERRED_HW_RENDER``.
         """
         ...
 
@@ -127,13 +195,23 @@ class VideoDriver(Protocol):
     @abstractmethod
     def set_context(self, callback: retro_hw_render_callback) -> retro_hw_render_callback | None:
         """
-        Corresponds to ``EnvironmentCall.SET_HW_RENDER``.
+        Requests the use of a particular graphics context,
+        including :attr:`.HardwareContext.NONE` to revert to software rendering.
 
-        :param callback:
-        :raises TypeError: If ``callback`` is not a ``retro_hw_render_callback``.
+        May not reset the video driver immediately;
+        may wait until the frame is finished before doing so.
+
+        :param callback: TODO
+
+        :raises TypeError: If ``callback`` is not a :class:`.retro_hw_render_callback`.
         :return: If the request in ``callback`` is accepted, a new ``retro_hw_render_callback``
           with values and pointers that the core can use to interact with the hardware context.
           Otherwise, ``None``.
+
+
+        .. note::
+
+            Corresponds to ``RETRO_ENVIRONMENT_SET_HW_RENDER``.
         """
         ...
 
