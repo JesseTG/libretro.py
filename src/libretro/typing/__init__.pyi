e7a43aed6f3dd07bb2791c1f37681c41a10d0e99
Jesse Talavera <jesse@jesse.tg>
2026-02-28 22:29:44 -0500

    Add some type declarations for `ctypes` compatibility

---
+++ src/libretro/typing/__init__.pyi
@@ -0,0 +1,71 @@
+from ctypes import *
+from typing import Any, Protocol
+
+from _ctypes import CFuncPtr, _CDataType, _Pointer
+
+CInt = (
+    c_int
+    | c_byte
+    | c_short
+    | c_long
+    | c_longlong
+    | c_int8
+    | c_int16
+    | c_int32
+    | c_int64
+    | c_ssize_t
+)
+CUint = (
+    c_uint
+    | c_ubyte
+    | c_ushort
+    | c_ulong
+    | c_ulonglong
+    | c_uint8
+    | c_uint16
+    | c_uint32
+    | c_uint64
+    | c_size_t
+)
+CReal = c_float | c_double | c_longdouble
+CNumber = CInt | CUint | CReal
+c_bool_arg = c_bool | bool | Any
+c_int_arg = c_int | int
+c_int16_arg = c_int16 | int
+c_int64_arg = c_int64 | int
+c_size_t_arg = c_size_t | int
+c_uint_arg = c_uint | int
+c_uint8_arg = c_uint8 | int
+c_uint32_arg = c_uint32 | int
+c_uint64_arg = c_uint64 | int
+c_str_arg = c_char_p | bytes
+type CIntArg[I: CInt | CUint] = I | int
+type CSignedIntArg[I: CInt] = I | int
+type CUnsignedIntArg[I: CUint] = I | int
+type CRealArg[R: CReal] = R | float
+type CBoolArg = bool | c_bool | Any
+type CBufferArg[T: _CDataType] = _Pointer[T] | memoryview | bytes
+type CPointerArg[T: _CDataType] = _Pointer[T] | None
+type CNullableBufferArg[T: _CDataType] = CBufferArg[T] | None
+type Pointer[T: _CDataType] = _Pointer[T]
+
+class TypedCFuncPtr[R: _CDataType | bool | int | float | bytes | None, **P](CFuncPtr):
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R: ...
+
+class AsParameter[T: _CDataType](Protocol):
+    _as_parameter_: T
+
+__all__ = (
+    "c_int_arg",
+    "CBufferArg",
+    "c_int16_arg",
+    "c_int64_arg",
+    "c_size_t_arg",
+    "c_bool_arg",
+    "c_uint_arg",
+    "Pointer",
+    "TypedCFuncPtr",
+    "c_uint8_arg",
+    "c_uint32_arg",
+    "c_uint64_arg",
+)
