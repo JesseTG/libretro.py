4faa9dd8a957e1de906a49d49bee08d9e10a7efc
Jesse Talavera <jesse@jesse.tg>
2026-03-09 15:55:20 -0400

    Add type hints to `libretro.error`

--- src/libretro/error.py
+++ src/libretro/error.py
@@ -1,10 +1,10 @@
 class UnsupportedEnvCall(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message: str, *args: object):
+        super().__init__(message, *args)
 
 
 class CoreShutDownException(Exception):
-    def __init__(self, *args):
+    def __init__(self, *args: object):
         super().__init__("Core has been shut down", *args)
 
 
