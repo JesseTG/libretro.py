14f70541ab8d606ed464a2b5845a40a7a863c6f9
Jesse Talavera <jesse@jesse.tg>
2026-03-20 14:51:08 -0400

    Fix types in `libretro.py.test.system_info`

--- src/libretro/py/test/system_info.py
+++ src/libretro/py/test/system_info.py
@@ -11,11 +11,17 @@ def main(libretro: CoreArg):
 
     system_info = libretro.get_system_info()
 
-    print(f"library_name:", system_info.library_name.decode())
-    print(f"library_version:", system_info.library_version.decode())
-    print(f"block_extract:", system_info.block_extract)
-    print(f"need_fullpath:", system_info.need_fullpath)
-    print(f"valid_extensions:", system_info.valid_extensions.decode())
+    library_name = system_info.library_name
+    library_version = system_info.library_version
+    block_extract = system_info.block_extract
+    need_fullpath = system_info.need_fullpath
+    valid_extensions = system_info.valid_extensions
+
+    print(f"library_name:", "NULL" if library_name is None else library_name.decode())
+    print(f"library_version:", "NULL" if library_version is None else library_version.decode())
+    print(f"block_extract:", block_extract)
+    print(f"need_fullpath:", need_fullpath)
+    print(f"valid_extensions:", "NULL" if valid_extensions is None else valid_extensions.decode())
 
 
 if __name__ == "__main__":
