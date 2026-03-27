e807ac5d5ba99318098debfe1c6af854bfa29e1e
Jesse Talavera <jesse@jesse.tg>
2026-03-27 19:46:48 -0400

    Remove an unused type

--- src/libretro/drivers/types.py
+++ src/libretro/drivers/types.py
@@ -1,21 +1,5 @@
 from abc import abstractmethod
-from typing import TYPE_CHECKING, Protocol, runtime_checkable
-
-if TYPE_CHECKING:
-    from ctypes import _CDataType
-
-
-@runtime_checkable
-class AsParameter[T: _CDataType](Protocol):
-    """
-    Protocol for objects that can be used as parameters in C functions.
-
-    :see: https://docs.python.org/3/library/ctypes.html#calling-functions-with-your-own-custom-data-types
-    """
-
-    @property
-    @abstractmethod
-    def _as_parameter_(self) -> T: ...
+from typing import Protocol, runtime_checkable
 
 
 @runtime_checkable
@@ -24,4 +8,4 @@ class Pollable(Protocol):
     def poll(self) -> None: ...
 
 
-__all__ = ("AsParameter", "Pollable")
+__all__ = ("Pollable",)
