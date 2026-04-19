34a8064a9ad48b28d4b68d9b8bda5cbc826eb127
Jesse Talavera <jesse@jesse.tg>
2026-04-18 21:09:29 -0400

    Flesh out docs for `libretro.api.log`

--- src/libretro/api/log.py
+++ src/libretro/api/log.py
@@ -1,3 +1,14 @@
+"""
+Types for allowing :class:`.Core`\\s to log messages.
+
+.. seealso::
+    :class:`.LogDriver`
+        The protocol that uses these types to implement logging support in libretro.py.
+
+    :mod:`libretro.drivers.log`
+        libretro.py's included :class:`.LogDriver` implementations.
+"""
+
 import logging
 from ctypes import Structure, c_int
 from dataclasses import dataclass
@@ -15,9 +26,25 @@ retro_log_level = c_int
 
 
 retro_log_printf_t = TypedFunctionPointer[None, [CIntArg[retro_log_level], CStringArg]]
+"""
+A :c:func:`printf`-style logging function.
+
+.. warning::
+    :c:type:`retro_log_printf_t` normally has :c:func:`printf`-style variadic arguments,
+    but :mod:`!ctypes` :python-issue:`doesn't currently support variadic callbacks <135620>`.
+
+    As a workaround, your :class:`.Core` can format log messages with :c:func:`sprintf` or similar,
+    then pass it as the format string.
+"""
 
 
 class LogLevel(IntEnum):
+    """
+    libretro's defined log severity levels.
+
+    Corresponds to :c:type:`retro_log_level` in ``libretro.h``.
+    """
+
     DEBUG = RETRO_LOG_DEBUG
     INFO = RETRO_LOG_INFO
     WARNING = RETRO_LOG_WARN
@@ -29,6 +56,13 @@ class LogLevel(IntEnum):
 
     @property
     def logging_level(self) -> int:
+        """Returns the equivalent :mod:`logging` level.
+
+        >>> import logging
+        >>> from libretro.api import LogLevel
+        >>> LogLevel.WARNING.logging_level == logging.WARN
+        True
+        """
         match self:
             case self.DEBUG:
                 return logging.DEBUG
@@ -42,11 +76,22 @@ class LogLevel(IntEnum):
 
 @dataclass(init=False, slots=True)
 class retro_log_callback(Structure):
+    """
+    Provides the :class:`.Core` with a logging function.
+
+    Corresponds to :c:type:`retro_log_callback` in ``libretro.h``.
+    """
+
     log: retro_log_printf_t | None
+    """A printf-style logging function. Set by the frontend."""
 
     _fields_ = (("log", retro_log_printf_t),)
 
     def __deepcopy__(self, _):
+        """
+        Returns a shallow copy of this object.
+        Intended for use with :func:`copy.deepcopy`.
+        """
         return retro_log_callback(self.log)
 
 
