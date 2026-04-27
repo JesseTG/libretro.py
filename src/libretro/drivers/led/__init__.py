56510906bf5ff508d59038b55bb18f60b3ab8b8b
Jesse Talavera <jesse@jesse.tg>
2026-04-27 19:51:46 -0400

    Flesh out the docs for `libretro.drivers.led`

--- src/libretro/drivers/led/__init__.py
+++ src/libretro/drivers/led/__init__.py
@@ -1,2 +1,11 @@
+"""
+Drivers that track the state of virtual LEDs exposed to the core.
+
+.. seealso::
+
+    :mod:`libretro.api.led`
+        Defines the LED interface structure that LED drivers implement.
+"""
+
 from .dict import *
 from .driver import *
