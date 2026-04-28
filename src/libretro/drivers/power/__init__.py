189c72fa06af559c60fd3cd9975b6a6c0d2690cd
Jesse Talavera <jesse@jesse.tg>
2026-04-27 21:14:24 -0400

    Touch up the docs for `libretro.drivers.power`

--- src/libretro/drivers/power/__init__.py
+++ src/libretro/drivers/power/__init__.py
@@ -1,2 +1,11 @@
+"""
+Drivers that report the device's power state to the core.
+
+.. seealso::
+
+    :mod:`libretro.api.power`
+        Defines the device power structure that power drivers report.
+"""
+
 from .constant import *
 from .driver import *
