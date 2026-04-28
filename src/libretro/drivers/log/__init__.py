266c22ce8c34e45e0aaab1645257802ad034e2be
Jesse Talavera <jesse@jesse.tg>
2026-04-27 21:12:20 -0400

    Flesh out the docs for `libretro.drivers.log`

--- src/libretro/drivers/log/__init__.py
+++ src/libretro/drivers/log/__init__.py
@@ -1,2 +1,11 @@
+"""
+Drivers that receive log output from the core.
+
+.. seealso::
+
+    :mod:`libretro.api.log`
+        Defines the log callback structure that log drivers implement.
+"""
+
 from .driver import *
 from .unformatted import *
--- src/libretro/drivers/log/driver.py
+++ src/libretro/drivers/log/driver.py
@@ -1,3 +1,12 @@
+"""
+Protocol definition for the log driver interface.
+
+.. seealso::
+
+    :mod:`libretro.api.log`
+        Provides the C callback structure that :class:`.LogDriver` implementations use.
+"""
+
 from abc import abstractmethod
 from typing import Protocol, runtime_checkable
 
@@ -6,9 +15,40 @@ from libretro.api.log import LogLevel
 
 @runtime_checkable
 class LogDriver(Protocol):
+    """
+    Protocol for drivers that receive log output from the core.
+
+    Cores request logging via ``RETRO_ENVIRONMENT_GET_LOG_INTERFACE``.
+
+    .. seealso::
+
+        :class:`~libretro.api.log.retro_log_callback`
+            The C callback struct whose ``log`` function pointer this protocol implements.
+    """
 
     @abstractmethod
-    def log(self, level: LogLevel, fmt: bytes) -> None: ...
+    def log(self, level: LogLevel, fmt: bytes) -> None:
+        """
+        Emits a log message from the core.
+
+        Corresponds to :c:type:`retro_log_printf_t`.
+
+        .. warning::
+            :c:type:`retro_log_printf_t` normally has :c:func:`printf`-style variadic arguments,
+            but :mod:`!ctypes` :python-issue:`doesn't currently support variadic callbacks <135620>`.
+
+            As a workaround, your :class:`.Core` can format log messages with :c:func:`sprintf` or similar,
+            then pass it as the format string.
+
+        :param level: The severity of the message.
+        :param fmt: The message as a null-terminated byte string.
+
+        .. seealso::
+
+            :attr:`.retro_log_callback.log`
+                The function pointer field in the callback struct that this method implements.
+        """
+        ...
 
 
 __all__ = ["LogDriver"]
