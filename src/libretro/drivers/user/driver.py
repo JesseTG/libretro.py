046b6424dcb1fff26afed5cb57f386b03d373e2a
Jesse Talavera <jesse@jesse.tg>
2026-04-27 22:23:23 -0400

    Flesh out docs for `libretro.drivers.user`

--- src/libretro/drivers/user/driver.py
+++ src/libretro/drivers/user/driver.py
@@ -1,3 +1,12 @@
+"""
+Protocol definition for the user driver interface.
+
+.. seealso::
+
+    :mod:`libretro.api.user`
+        Provides the language and username types that :class:`.UserDriver` implementations supply.
+"""
+
 from abc import abstractmethod
 from typing import Protocol, runtime_checkable
 
@@ -6,13 +15,37 @@ from libretro.api.user import Language
 
 @runtime_checkable
 class UserDriver(Protocol):
-    @property
-    @abstractmethod
-    def username(self) -> bytes | None: ...
+    """
+    Protocol for drivers that supply player identity information to the core.
+
+    Cores query this information via ``RETRO_ENVIRONMENT_GET_USERNAME``
+    and ``RETRO_ENVIRONMENT_GET_LANGUAGE``.
+
+    .. seealso::
+
+        :mod:`libretro.api.user`
+            The language enumeration and username types that implementations of this protocol supply.
+    """
 
     @property
     @abstractmethod
-    def language(self) -> Language | None: ...
+    def username(self) -> bytes | None:
+        """
+        The player username exposed to the core via ``RETRO_ENVIRONMENT_GET_USERNAME``.
+
+        :obj:`None` disables the environment call.
+        """
+        ...
+
+    @property
+    @abstractmethod
+    def language(self) -> Language | None:
+        """
+        The UI language exposed to the core via ``RETRO_ENVIRONMENT_GET_LANGUAGE``.
+
+        :obj:`None` disables the environment call.
+        """
+        ...
 
 
 __all__ = ["UserDriver"]
